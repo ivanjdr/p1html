ab27330cc6bb546387a489df750baa20843f48ac
ivanjdr <juarezdavilaricardo1@gmail.com>
2024-02-09 12:10:37 -0600

    Se agrego un nuevo funcionamiento al boton y un formulario

---
+++ JAVASCRIPT/functions.js
@@ -0,0 +1,11 @@
+function changeColor(){
+  document.getElementById("palabrita").style.backgroundColor="green";
+var tamaño = document.getElementById("palabra");
+    tamaño.style.fontSize = 40;
+document.getElementById("palabrita").textContent="hola";
+ 
+}
+ 
+function enviarMsg(){
+  alert("Hola amigo");
+}
